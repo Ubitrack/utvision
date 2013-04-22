7d223c557bb168fc333c54376390e7803c2b7ecb
Frieder Pankratz <pankratz@in.tum.de>
2013-04-22 18:43:29 +0200

    now with boost_filesystem_version 3

--- src/utVision/MarkerBundle.cpp
+++ src/utVision/MarkerBundle.cpp
@@ -788,8 +788,13 @@ void createImageList( std::vector< std::string >& l )
 		std::string suffix = ".jpg";
 		
 		// check if file of suitable extension
+#if BOOST_FILESYSTEM_VERSION == 3
+		if ( exists( p ) && !is_directory( p ) && regex_match( p.leaf().string(), ext ) )
+			l.push_back( p.leaf().string() );
+#else
 		if ( exists( p ) && !is_directory( p ) && regex_match( p.leaf(), ext ) )
 			l.push_back( p.leaf() );
+#endif
 	}
 
 }
