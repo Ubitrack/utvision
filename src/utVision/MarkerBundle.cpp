f826e13464c463b8e0cd3c0c27f8e26c1e90aaec
FAR <far@ubiAndroid.(none)>
2013-06-23 20:27:38 +0200

    android and linux

--- src/utVision/MarkerBundle.cpp
+++ src/utVision/MarkerBundle.cpp
@@ -770,8 +770,12 @@ void createImageList( std::vector< std::string >& l )
 	using namespace boost::filesystem;
 	
 	boost::regex ext( ".*\\.(jpg|JPG|png|PNG|bmp|BMP)" );
-	
+
+#if BOOST_FILESYSTEM_VERSION == 3	
+	path compPath( ".");
+#else
 	path compPath( ".", native );
+#endif
 	if ( !exists( compPath ) )
 		throw std::string( "path does not exist" );
 	
