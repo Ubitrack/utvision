ef46d85b926e8a2304fb35ef8c281777e388a249
Ulrich Eck <ueck@net-labs.de>
2018-03-22 20:51:18 +0100

    added header comments

--- src/utVision/Config_cmake.hh
+++ src/utVision/Config_cmake.hh
@@ -1,4 +1,34 @@
-#ifndef __UBITRACK_VISION_CONFIG_H_INCLUDED__
+/*
+ * Ubitrack - Library for Ubiquitous Tracking
+ * Copyright 2006, Technische Universitaet Muenchen, and individual
+ * contributors as indicated by the @authors tag. See the 
+ * copyright.txt in the distribution for a full listing of individual
+ * contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+/**
+ * @file
+ * Core definitions of the ubitrack vision library
+ *
+ * @author Ulrich Eck <ulrich.eck@tum.de>
+ */
+
+ #ifndef __UBITRACK_VISION_CONFIG_H_INCLUDED__
 #define __UBITRACK_VISION_CONFIG_H_INCLUDED__
 
 #ifndef HAVE_GLAD
