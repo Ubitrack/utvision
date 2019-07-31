e429d4b3e077f8bfbbc4c61db8ccc1d07cc19b01
Frieder Pankratz <pankratz@in.tum.de>
2019-07-31 17:56:34 +0200

    first working conan workspace version

--- conanfile.py
+++ conanfile.py
@@ -38,7 +38,7 @@ class UbitrackCoreConan(ConanFile):
     def requirements(self):
         userChannel = "ubitrack/stable"
         if self.options.workspaceBuild:
-            userChannel = "user/testing"
+            userChannel = "local/dev"
         self.requires("ubitrack_core/%s@%s" % (self.version, userChannel))
         self.requires("opencv/[>=3.2.0]@camposs/stable" )
 
